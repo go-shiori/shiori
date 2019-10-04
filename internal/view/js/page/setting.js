3077c7fbb880a4fd8632f10ee84a9be5d065bbbc
Radhi Fadlillah <m.radhi.f@gmail.com>
2019-10-04 16:51:08 +0700

    Use tab instead of spaces

--- internal/view/js/page/setting.js
+++ internal/view/js/page/setting.js
@@ -70,235 +70,235 @@ import customDialog from "../component/dialog.js";
 import basePage from "./base.js";
 
 export default {
-    template: template,
-    mixins: [basePage],
-    components: {
-        customDialog
-    },
-    data() {
-        return {
-            loading: false,
-            accounts: []
-        }
-    },
-    methods: {
-        saveSetting() {
-            this.$emit("setting-changed", {
-                showId: this.appOptions.showId,
-                listMode: this.appOptions.listMode,
-                hideThumbnail: this.appOptions.hideThumbnail,
-                hideExcerpt: this.appOptions.hideExcerpt,
-                nightMode: this.appOptions.nightMode,
-                keepMetadata: this.appOptions.keepMetadata,
-                useArchive: this.appOptions.useArchive,
-                makePublic: this.appOptions.makePublic,
-            });
-        },
-        loadAccounts() {
-            if (this.loading) return;
+	template: template,
+	mixins: [basePage],
+	components: {
+		customDialog
+	},
+	data() {
+		return {
+			loading: false,
+			accounts: []
+		}
+	},
+	methods: {
+		saveSetting() {
+			this.$emit("setting-changed", {
+				showId: this.appOptions.showId,
+				listMode: this.appOptions.listMode,
+				hideThumbnail: this.appOptions.hideThumbnail,
+				hideExcerpt: this.appOptions.hideExcerpt,
+				nightMode: this.appOptions.nightMode,
+				keepMetadata: this.appOptions.keepMetadata,
+				useArchive: this.appOptions.useArchive,
+				makePublic: this.appOptions.makePublic,
+			});
+		},
+		loadAccounts() {
+			if (this.loading) return;
 
-            this.loading = true;
-            fetch("/api/accounts")
-                .then(response => {
-                    if (!response.ok) throw response;
-                    return response.json();
-                })
-                .then(json => {
-                    this.loading = false;
-                    this.accounts = json;
-                })
-                .catch(err => {
-                    this.loading = false;
-                    this.getErrorMessage(err).then(msg => {
-                        this.showErrorDialog(msg);
-                    })
-                });
-        },
-        showDialogNewAccount() {
-            this.showDialog({
-                title: "New Account",
-                content: "Input new account's data :",
-                fields: [{
-                    name: "username",
-                    label: "Username",
-                    value: "",
-                }, {
-                    name: "password",
-                    label: "Password",
-                    type: "password",
-                    value: "",
-                }, {
-                    name: "repeat",
-                    label: "Repeat password",
-                    type: "password",
-                    value: "",
-                }, {
-                    name: "visitor",
-                    label: "This account is for visitor",
-                    type: "check",
-                    value: false,
-                }],
-                mainText: "OK",
-                secondText: "Cancel",
-                mainClick: (data) => {
-                    if (data.username === "") {
-                        this.showErrorDialog("Username must not empty");
-                        return;
-                    }
+			this.loading = true;
+			fetch("/api/accounts")
+				.then(response => {
+					if (!response.ok) throw response;
+					return response.json();
+				})
+				.then(json => {
+					this.loading = false;
+					this.accounts = json;
+				})
+				.catch(err => {
+					this.loading = false;
+					this.getErrorMessage(err).then(msg => {
+						this.showErrorDialog(msg);
+					})
+				});
+		},
+		showDialogNewAccount() {
+			this.showDialog({
+				title: "New Account",
+				content: "Input new account's data :",
+				fields: [{
+					name: "username",
+					label: "Username",
+					value: "",
+				}, {
+					name: "password",
+					label: "Password",
+					type: "password",
+					value: "",
+				}, {
+					name: "repeat",
+					label: "Repeat password",
+					type: "password",
+					value: "",
+				}, {
+					name: "visitor",
+					label: "This account is for visitor",
+					type: "check",
+					value: false,
+				}],
+				mainText: "OK",
+				secondText: "Cancel",
+				mainClick: (data) => {
+					if (data.username === "") {
+						this.showErrorDialog("Username must not empty");
+						return;
+					}
 
-                    if (data.password === "") {
-                        this.showErrorDialog("Password must not empty");
-                        return;
-                    }
+					if (data.password === "") {
+						this.showErrorDialog("Password must not empty");
+						return;
+					}
 
-                    if (data.password !== data.repeat) {
-                        this.showErrorDialog("Password does not match");
-                        return;
-                    }
+					if (data.password !== data.repeat) {
+						this.showErrorDialog("Password does not match");
+						return;
+					}
 
-                    var request = {
-                        username: data.username,
-                        password: data.password,
-                        owner: !data.visitor,
-                    }
+					var request = {
+						username: data.username,
+						password: data.password,
+						owner: !data.visitor,
+					}
 
-                    this.dialog.loading = true;
-                    fetch("/api/accounts", {
-                        method: "post",
-                        body: JSON.stringify(request),
-                        headers: {
-                            "Content-Type": "application/json",
-                        }
-                    }).then(response => {
-                        if (!response.ok) throw response;
-                        return response;
-                    }).then(() => {
-                        this.dialog.loading = false;
-                        this.dialog.visible = false;
+					this.dialog.loading = true;
+					fetch("/api/accounts", {
+						method: "post",
+						body: JSON.stringify(request),
+						headers: {
+							"Content-Type": "application/json",
+						}
+					}).then(response => {
+						if (!response.ok) throw response;
+						return response;
+					}).then(() => {
+						this.dialog.loading = false;
+						this.dialog.visible = false;
 
-                        this.accounts.push({ username: data.username, owner: !data.visitor });
-                        this.accounts.sort((a, b) => {
-                            var nameA = a.username.toLowerCase(),
-                                nameB = b.username.toLowerCase();
+						this.accounts.push({ username: data.username, owner: !data.visitor });
+						this.accounts.sort((a, b) => {
+							var nameA = a.username.toLowerCase(),
+								nameB = b.username.toLowerCase();
 
-                            if (nameA < nameB) {
-                                return -1;
-                            }
+							if (nameA < nameB) {
+								return -1;
+							}
 
-                            if (nameA > nameB) {
-                                return 1;
-                            }
+							if (nameA > nameB) {
+								return 1;
+							}
 
-                            return 0;
-                        });
-                    }).catch(err => {
-                        this.dialog.loading = false;
-                        this.getErrorMessage(err).then(msg => {
-                            this.showErrorDialog(msg);
-                        })
-                    });
-                }
-            });
-        },
-        showDialogChangePassword(account) {
-            this.showDialog({
-                title: "Change Password",
-                content: "Input new password :",
-                fields: [{
-                    name: "oldPassword",
-                    label: "Old password",
-                    type: "password",
-                    value: "",
-                }, {
-                    name: "password",
-                    label: "New password",
-                    type: "password",
-                    value: "",
-                }, {
-                    name: "repeat",
-                    label: "Repeat password",
-                    type: "password",
-                    value: "",
-                }],
-                mainText: "OK",
-                secondText: "Cancel",
-                mainClick: (data) => {
-                    if (data.oldPassword === "") {
-                        this.showErrorDialog("Old password must not empty");
-                        return;
-                    }
+							return 0;
+						});
+					}).catch(err => {
+						this.dialog.loading = false;
+						this.getErrorMessage(err).then(msg => {
+							this.showErrorDialog(msg);
+						})
+					});
+				}
+			});
+		},
+		showDialogChangePassword(account) {
+			this.showDialog({
+				title: "Change Password",
+				content: "Input new password :",
+				fields: [{
+					name: "oldPassword",
+					label: "Old password",
+					type: "password",
+					value: "",
+				}, {
+					name: "password",
+					label: "New password",
+					type: "password",
+					value: "",
+				}, {
+					name: "repeat",
+					label: "Repeat password",
+					type: "password",
+					value: "",
+				}],
+				mainText: "OK",
+				secondText: "Cancel",
+				mainClick: (data) => {
+					if (data.oldPassword === "") {
+						this.showErrorDialog("Old password must not empty");
+						return;
+					}
 
-                    if (data.password === "") {
-                        this.showErrorDialog("New password must not empty");
-                        return;
-                    }
+					if (data.password === "") {
+						this.showErrorDialog("New password must not empty");
+						return;
+					}
 
-                    if (data.password !== data.repeat) {
-                        this.showErrorDialog("Password does not match");
-                        return;
-                    }
+					if (data.password !== data.repeat) {
+						this.showErrorDialog("Password does not match");
+						return;
+					}
 
-                    var request = {
-                        username: account.username,
-                        oldPassword: data.oldPassword,
-                        newPassword: data.password,
-                        owner: account.owner,
-                    }
+					var request = {
+						username: account.username,
+						oldPassword: data.oldPassword,
+						newPassword: data.password,
+						owner: account.owner,
+					}
 
-                    this.dialog.loading = true;
-                    fetch("/api/accounts", {
-                        method: "put",
-                        body: JSON.stringify(request),
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    }).then(response => {
-                        if (!response.ok) throw response;
-                        return response;
-                    }).then(() => {
-                        this.dialog.loading = false;
-                        this.dialog.visible = false;
-                    }).catch(err => {
-                        this.dialog.loading = false;
-                        this.getErrorMessage(err).then(msg => {
-                            this.showErrorDialog(msg);
-                        })
-                    });
-                }
-            });
-        },
-        showDialogDeleteAccount(account, idx) {
-            this.showDialog({
-                title: "Delete Account",
-                content: `Delete account "${account.username}" ?`,
-                mainText: "Yes",
-                secondText: "No",
-                mainClick: () => {
-                    this.dialog.loading = true;
-                    fetch(`/api/accounts`, {
-                        method: "delete",
-                        body: JSON.stringify([account.username]),
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    }).then(response => {
-                        if (!response.ok) throw response;
-                        return response;
-                    }).then(() => {
-                        this.dialog.loading = false;
-                        this.dialog.visible = false;
-                        this.accounts.splice(idx, 1);
-                    }).catch(err => {
-                        this.dialog.loading = false;
-                        this.getErrorMessage(err).then(msg => {
-                            this.showErrorDialog(msg);
-                        })
-                    });
-                }
-            });
-        },
-    },
-    mounted() {
-        this.loadAccounts();
-    }
+					this.dialog.loading = true;
+					fetch("/api/accounts", {
+						method: "put",
+						body: JSON.stringify(request),
+						headers: {
+							"Content-Type": "application/json",
+						},
+					}).then(response => {
+						if (!response.ok) throw response;
+						return response;
+					}).then(() => {
+						this.dialog.loading = false;
+						this.dialog.visible = false;
+					}).catch(err => {
+						this.dialog.loading = false;
+						this.getErrorMessage(err).then(msg => {
+							this.showErrorDialog(msg);
+						})
+					});
+				}
+			});
+		},
+		showDialogDeleteAccount(account, idx) {
+			this.showDialog({
+				title: "Delete Account",
+				content: `Delete account "${account.username}" ?`,
+				mainText: "Yes",
+				secondText: "No",
+				mainClick: () => {
+					this.dialog.loading = true;
+					fetch(`/api/accounts`, {
+						method: "delete",
+						body: JSON.stringify([account.username]),
+						headers: {
+							"Content-Type": "application/json",
+						},
+					}).then(response => {
+						if (!response.ok) throw response;
+						return response;
+					}).then(() => {
+						this.dialog.loading = false;
+						this.dialog.visible = false;
+						this.accounts.splice(idx, 1);
+					}).catch(err => {
+						this.dialog.loading = false;
+						this.getErrorMessage(err).then(msg => {
+							this.showErrorDialog(msg);
+						})
+					});
+				}
+			});
+		},
+	},
+	mounted() {
+		this.loadAccounts();
+	}
 }
