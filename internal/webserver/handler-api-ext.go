52e53e7ea1eeabb4cbee96f12bdb146767274f1f
hulb <hulb@live.cn>
2020-03-07 17:02:04 +0800

    use goroutine to handle heavy process

--- internal/webserver/handler-api-ext.go
+++ internal/webserver/handler-api-ext.go
@@ -34,76 +34,87 @@ func (h *handler) apiInsertViaExtension(w http.ResponseWriter, r *http.Request, 
 
 	// Check if bookmark already exists.
 	book, exist := h.DB.GetBookmark(0, request.URL)
-
-	// If it already exists, we need to set ID and tags.
-	if exist {
-		book.HTML = request.HTML
-
-		mapOldTags := map[string]model.Tag{}
-		for _, oldTag := range book.Tags {
-			mapOldTags[oldTag.Name] = oldTag
-		}
-
-		for _, newTag := range request.Tags {
-			if _, tagExist := mapOldTags[newTag.Name]; !tagExist {
-				book.Tags = append(book.Tags, newTag)
-			}
-		}
-	} else {
+	if !exist {
+		// save the bookmark first and process it in a goroutine
 		book = request
+		if book.Title == "" {
+			book.Title = "unprocessed"
+		}
+
 		book.ID, err = h.DB.CreateNewID("bookmark")
 		if err != nil {
 			panic(fmt.Errorf("failed to create ID: %v", err))
 		}
-	}
 
-	// Since we are using extension, the extension might send the HTML content
-	// so no need to download it again here. However, if it's empty, it might be not HTML file
-	// so we download it here.
-	var contentType string
-	var contentBuffer io.Reader
-
-	if book.HTML == "" {
-		contentBuffer, contentType, _ = core.DownloadBookmark(book.URL)
-	} else {
-		contentType = "text/html; charset=UTF-8"
-		contentBuffer = bytes.NewBufferString(book.HTML)
-	}
-
-	// At this point the web page already downloaded.
-	// Time to process it.
-	if contentBuffer != nil {
-		book.CreateArchive = true
-		request := core.ProcessRequest{
-			DataDir:     h.DataDir,
-			Bookmark:    book,
-			Content:     contentBuffer,
-			ContentType: contentType,
-		}
-
-		var isFatalErr bool
-		book, isFatalErr, err = core.ProcessBookmark(request)
-
-		if tmp, ok := contentBuffer.(io.ReadCloser); ok {
-			tmp.Close()
-		}
-
-		if err != nil && isFatalErr {
-			panic(fmt.Errorf("failed to process bookmark: %v", err))
+		results, err := h.DB.SaveBookmarks(book)
+		if err != nil || len(results) == 0 {
+			panic(fmt.Errorf("failed to save bookmark: %v", err))
 		}
 	}
 
-	// Save bookmark to database
-	results, err := h.DB.SaveBookmarks(book)
-	if err != nil || len(results) == 0 {
-		panic(fmt.Errorf("failed to save bookmark: %v", err))
-	}
-	book = results[0]
+	go func() {
+		// If it already exists, we need to set ID and tags.
+		if exist {
+			book.HTML = request.HTML
 
-	// Return the new bookmark
+			mapOldTags := map[string]model.Tag{}
+			for _, oldTag := range book.Tags {
+				mapOldTags[oldTag.Name] = oldTag
+			}
+
+			for _, newTag := range request.Tags {
+				if _, tagExist := mapOldTags[newTag.Name]; !tagExist {
+					book.Tags = append(book.Tags, newTag)
+				}
+			}
+		}
+
+		// Since we are using extension, the extension might send the HTML content
+		// so no need to download it again here. However, if it's empty, it might be not HTML file
+		// so we download it here.
+		var contentType string
+		var contentBuffer io.Reader
+
+		if book.HTML == "" {
+			contentBuffer, contentType, _ = core.DownloadBookmark(book.URL)
+		} else {
+			contentType = "text/html; charset=UTF-8"
+			contentBuffer = bytes.NewBufferString(book.HTML)
+		}
+
+		// At this point the web page already downloaded.
+		// Time to process it.
+		if contentBuffer != nil {
+			book.CreateArchive = true
+			request := core.ProcessRequest{
+				DataDir:     h.DataDir,
+				Bookmark:    book,
+				Content:     contentBuffer,
+				ContentType: contentType,
+			}
+
+			var isFatalErr bool
+			fmt.Printf("url: [%s] is processing...", book.URL)
+			book, isFatalErr, err = core.ProcessBookmark(request)
+			fmt.Printf("done\n")
+
+			if tmp, ok := contentBuffer.(io.ReadCloser); ok {
+				tmp.Close()
+			}
+
+			if err != nil && isFatalErr {
+				panic(fmt.Errorf("failed to process bookmark: %v", err))
+			}
+		}
+
+		// Save bookmark to database
+		results, err := h.DB.SaveBookmarks(book)
+		if err != nil || len(results) == 0 {
+			panic(fmt.Errorf("failed to save bookmark: %v", err))
+		}
+	}()
+
 	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(&book)
-	checkError(err)
 }
 
 // apiDeleteViaExtension is handler for DELETE /api/bookmark/ext
