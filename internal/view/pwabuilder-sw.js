47a65fc2c113526bdcbd7677574998c53abf82cf
Aaron Ogle <aaron@geekgonecrazy.com>
2022-01-30 15:28:30 -0600

    cache api and html so actually works offline

--- internal/view/pwabuilder-sw.js
+++ internal/view/pwabuilder-sw.js
@@ -1,8 +1,14 @@
-// This is the "Offline copy of assets" service worker
-
-const CACHE = "pwabuilder-offline";
+//This is the service worker with the Advanced caching
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const API_CACHE = "api";
+const BOOKMARK_CACHE = "bookmarks";
+const HTML_CACHE = "html";
+const JS_CACHE = "javascript";
+const STYLE_CACHE = "stylesheets";
+const IMAGE_CACHE = "images";
+const FONT_CACHE = "fonts";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -11,8 +17,86 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  ({url, request, event}) => url.pathname === '/api/bookmarks',
+  new workbox.strategies.NetworkFirst({
+    cacheName: API_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({url, event}) => event.request.destination === 'document' && url.pathname.startsWith('/bookmark'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: BOOKMARK_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 500,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({url, event}) => event.request.destination === 'document' && !url.pathname.startsWith('/bookmark'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: HTML_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 10,
+      }),
+    ],
+  })
+);
+
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'script',
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
+    cacheName: JS_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: STYLE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'image',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: IMAGE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'font',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: FONT_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
   })
 );
