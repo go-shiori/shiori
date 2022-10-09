158c52a3253a2ef437f1b8c2eb031f5b72cb5d3f
Felipe Martin Garcia <812088+fmartingr@users.noreply.github.com>
2022-10-09 18:50:42 +0200

    fix: data dir permissions having execute for all (#493)

--- internal/cmd/export.go
+++ internal/cmd/export.go
@@ -38,7 +38,7 @@ func exportHandler(cmd *cobra.Command, args []string) {
 
 	// Make sure destination directory exist
 	dstDir := fp.Dir(args[0])
-	if err := os.MkdirAll(dstDir, os.ModePerm); err != nil {
+	if err := os.MkdirAll(dstDir, model.DataDirPerm); err != nil {
 		cError.Printf("Error crating destination directory: %s", err)
 	}
 
--- internal/model/const.go
+++ internal/model/const.go
@@ -1,3 +1,7 @@
 package model
 
+// DataDirPerm the default filesystem permissions for the data directory/archives
+const DataDirPerm = 0744
+
+// DatabaseDateFormat the string formatting of datetimes for the database
 const DatabaseDateFormat = "2006-01-02 15:04:05"
