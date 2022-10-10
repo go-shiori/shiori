29d0de4a55ad91a7bf3e1a3219af3d7c877b7235
Felipe M <me@fmartingr.com>
2022-10-10 15:53:39 +0200

    Added shortcut to send internal server error response

--- internal/http/response/shortcuts.go
+++ internal/http/response/shortcuts.go
@@ -2,6 +2,8 @@ package response
 
 import "github.com/gofiber/fiber/v2"
 
+const internalServerErrorMessage = "Internal server error, please contact an administrator"
+
 // New provides a shortcut to a successful response object
 func New(ok bool, statusCode int, data interface{}) *Response {
 	return NewResponse(ok, data, nil, statusCode)
@@ -20,4 +22,9 @@ func SendError(ctx *fiber.Ctx, statusCode int, data interface{}) error {
 // SendErrorWithParams the same as above but for errors that require error parameters
 func SendErrorWithParams(ctx *fiber.Ctx, statusCode int, data interface{}, errorParams map[string]string) error {
 	return NewResponse(false, data, errorParams, statusCode).Send(ctx)
+}
+
+// SendInternalServerError directly sends an internal server error response
+func SendInternalServerError(ctx *fiber.Ctx) error {
+	return SendError(ctx, fiber.StatusInternalServerError, internalServerErrorMessage)
 }
