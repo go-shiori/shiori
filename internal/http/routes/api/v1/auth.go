6bdb10608708064fa37c5488ab5df5c78641ae9a
monirzadeh <25131576+Monirzadeh@users.noreply.github.com>
2023-07-25 12:06:22 +0330

    return PUT

--- internal/http/routes/api/v1/auth.go
+++ internal/http/routes/api/v1/auth.go
@@ -23,7 +23,7 @@ func (r *AuthAPIRoutes) Setup(group *gin.RouterGroup) model.Routes {
 	group.GET("/me", r.meHandler)
 	group.POST("/login", r.loginHandler)
 	group.POST("/refresh", r.refreshHandler)
-	group.PATCH("/account", r.settingsHandler)
+	group.PUT("/account", r.settingsHandler)
 	return r
 }
 
@@ -186,7 +186,6 @@ func (r *AuthAPIRoutes) settingsHandler(c *gin.Context) {
 		return
 	}
 	account.Config = payload.Config
-	//fmt.Println(account)
 
 	err = r.deps.Database.SaveAccountSettings(c, account)
 	if err != nil {
