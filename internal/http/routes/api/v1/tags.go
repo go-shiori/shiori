63faeaa5aa3845ffb13f1f49fdd5110ee596ae71
Felipe M <me@fmartingr.com>
2023-08-06 20:49:26 +0200

    test

--- internal/http/routes/api/v1/tags.go
+++ internal/http/routes/api/v1/tags.go
@@ -21,13 +21,13 @@ func (r *TagsAPIRoutes) Setup(g *gin.RouterGroup) model.Routes {
 	return r
 }
 
-//	@Summary					List tags
-//	@Tags						Tags
-//	@securityDefinitions.apikey	ApiKeyAuth
-//	@Produce					json
-//	@Success					200	{object}	model.Tag	"List of tags"
-//	@Failure					403	{object}	nil			"Token not provided/invalid"
-//	@Router						/api/v1/tags [get]
+// @Summary					List tags
+// @Tags						Tags2
+// @securityDefinitions.apikey	ApiKeyAuth
+// @Produce					json
+// @Success					200	{object}	model.Tag	"List of tags"
+// @Failure					403	{object}	nil			"Token not provided/invalid"
+// @Router						/api/v1/tags [get]
 func (r *TagsAPIRoutes) listHandler(c *gin.Context) {
 	tags, err := r.deps.Database.GetTags(c)
 	if err != nil {
@@ -38,14 +38,14 @@ func (r *TagsAPIRoutes) listHandler(c *gin.Context) {
 	response.Send(c, http.StatusOK, tags)
 }
 
-//	@Summary					Create tag
-//	@Tags						Tags
-//	@securityDefinitions.apikey	ApiKeyAuth
-//	@Produce					json
-//	@Success					200	{object}	model.Tag	"Created tag"
-//	@Failure					400	{object}	nil			"Token not provided/invalid"
-//	@Failure					403	{object}	nil			"Token not provided/invalid"
-//	@Router						/api/v1/tags [post]
+// @Summary					Create tag
+// @Tags						Tags
+// @securityDefinitions.apikey	ApiKeyAuth
+// @Produce					json
+// @Success					200	{object}	model.Tag	"Created tag"
+// @Failure					400	{object}	nil			"Token not provided/invalid"
+// @Failure					403	{object}	nil			"Token not provided/invalid"
+// @Router						/api/v1/tags [post]
 func (r *TagsAPIRoutes) createHandler(c *gin.Context) {
 	var tag model.Tag
 	if err := c.BindJSON(&tag); err != nil {
