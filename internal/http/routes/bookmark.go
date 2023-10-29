1d58455b53150f9e0f09d46ffac968b7f47033bf
Felipe M. <me@fmartingr.com>
2023-10-29 15:42:57 +0100

    chore: comment unused functions

--- internal/http/routes/bookmark.go
+++ internal/http/routes/bookmark.go
@@ -27,42 +27,42 @@ func (r *BookmarkRoutes) Setup(group *gin.RouterGroup) model.Routes {
 	return r
 }
 
-func (r *BookmarkRoutes) bookmarkContentHandler(c *gin.Context) {
-	ctx := context.NewContextFromGin(c)
+// func (r *BookmarkRoutes) bookmarkContentHandler(c *gin.Context) {
+// 	ctx := context.NewContextFromGin(c)
 
-	bookmarkIDParam, present := c.Params.Get("id")
-	if !present {
-		response.SendError(c, 400, "Invalid bookmark ID")
-		return
-	}
+// 	bookmarkIDParam, present := c.Params.Get("id")
+// 	if !present {
+// 		response.SendError(c, 400, "Invalid bookmark ID")
+// 		return
+// 	}
 
-	bookmarkID, err := strconv.Atoi(bookmarkIDParam)
-	if err != nil {
-		r.logger.WithError(err).Error("error parsing bookmark ID parameter")
-		response.SendInternalServerError(c)
-		return
-	}
+// 	bookmarkID, err := strconv.Atoi(bookmarkIDParam)
+// 	if err != nil {
+// 		r.logger.WithError(err).Error("error parsing bookmark ID parameter")
+// 		response.SendInternalServerError(c)
+// 		return
+// 	}
 
-	if bookmarkID == 0 {
-		response.SendError(c, 404, nil)
-		return
-	}
+// 	if bookmarkID == 0 {
+// 		response.SendError(c, 404, nil)
+// 		return
+// 	}
 
-	bookmark, found, err := r.deps.Database.GetBookmark(c, bookmarkID, "")
-	if err != nil || !found {
-		response.SendError(c, 404, nil)
-		return
-	}
+// 	bookmark, found, err := r.deps.Database.GetBookmark(c, bookmarkID, "")
+// 	if err != nil || !found {
+// 		response.SendError(c, 404, nil)
+// 		return
+// 	}
 
-	if bookmark.Public != 1 && !ctx.UserIsLogged() {
-		response.SendError(c, http.StatusForbidden, nil)
-		return
-	}
+// 	if bookmark.Public != 1 && !ctx.UserIsLogged() {
+// 		response.SendError(c, http.StatusForbidden, nil)
+// 		return
+// 	}
 
-	response.Send(c, 200, bookmark.Content)
-}
+// 	response.Send(c, 200, bookmark.Content)
+// }
 
-func (r *BookmarkRoutes) bookmarkArchiveHandler(c *gin.Context) {}
+// func (r *BookmarkRoutes) bookmarkArchiveHandler(c *gin.Context) {}
 
 func NewBookmarkRoutes(logger *logrus.Logger, deps *config.Dependencies) *BookmarkRoutes {
 	return &BookmarkRoutes{
