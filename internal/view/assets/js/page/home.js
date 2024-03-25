5a166906db813917b4db23f410d929d499ad5365
Zlendy <zlendy@proton.me>
2024-03-25 23:54:55 +0100

    fix: use correct endpoint in rename tags operation (#868)

--- internal/view/assets/js/page/home.js
+++ internal/view/assets/js/page/home.js
@@ -953,7 +953,7 @@ export default {
 					};
 
 					this.dialog.loading = true;
-					fetch(new URL("api/tag", document.baseURI), {
+					fetch(new URL("api/tags", document.baseURI), {
 						method: "PUT",
 						body: JSON.stringify(newData),
 						headers: {
