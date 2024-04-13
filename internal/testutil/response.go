f9311669579cfdc6b58a31a19aaea9c582c1a81f
Felipe M. <me@fmartingr.com>
2024-04-13 17:42:03 +0200

    testutil improvements

--- internal/testutil/response.go
+++ internal/testutil/response.go
@@ -26,27 +26,35 @@ func (r testResponse) AssertMessageEquals(t *testing.T, expected interface{}) {
 	require.Equal(t, expected, r.Response.GetMessage())
 }
 
-func (r *testResponse) AssertOk(t *testing.T) {
-	require.True(t, !r.Response.IsError())
+func (r *testResponse) AssertMessageIsListLength(t *testing.T, length int) {
+	require.Len(t, r.Response.GetMessage(), length)
 }
 
-func (r *testResponse) AssertNotOk(t *testing.T) {
+func (r *testResponse) AssertOk(t *testing.T) {
 	require.False(t, r.Response.IsError())
 }
 
+func (r *testResponse) AssertNotOk(t *testing.T) {
+	require.True(t, r.Response.IsError())
+}
+
+func (r *testResponse) Assert(t *testing.T, fn func(t *testing.T, r *testResponse)) {
+	fn(t, r)
+}
+
 func NewTestResponseFromBytes(b []byte) (*testResponse, error) {
-	r := testResponse{}
-	if err := json.Unmarshal(b, &r.Response); err != nil {
+	tr := testResponse{}
+	if err := json.Unmarshal(b, &tr.Response); err != nil {
 		return nil, errors.Wrap(err, "error parsing response")
 	}
-	return &r, nil
+	return &tr, nil
 }
 
 func NewTestResponseFromReader(r io.Reader) (*testResponse, error) {
-	response := testResponse{}
+	tr := testResponse{}
 	decoder := json.NewDecoder(r)
-	if err := decoder.Decode(&response.Response); err != nil {
+	if err := decoder.Decode(&tr.Response); err != nil {
 		return nil, errors.Wrap(err, "error parsing response")
 	}
-	return &response, nil
+	return &tr, nil
 }
