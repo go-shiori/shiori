860d20fdb9b0341f18fd92119678ddba0cb88cb6
monirzadeh <25131576+Monirzadeh@users.noreply.github.com>
2024-05-26 02:21:03 +0330

    change enviroment variable to just BUN

--- scripts/styles.sh
+++ scripts/styles.sh
@@ -10,21 +10,21 @@ OUTPUT_ARCHIVECSS=internal/view/assets/css/archive.css
 
 if [ -x "$(command -v bun)" ]; then
     if grep -q avx2 /proc/cpuinfo; then
-        BUNEXCUTE="bun"
+        BUN="bun"
     else
-        BUNEXCUTE="sde -chip-check-disable -- bun"
+        BUN="sde -chip-check-disable -- bun"
         echo "Your cpu not support avx2 so We use sde for more information please lookat https://github.com/oven-sh/bun/issues/762#issuecomment-1186505847"
     fi
 fi
 
 # Use bun is installled
 if [ -x "$(command -v bun)" ]; then
-    $BUNEXCUTE install
-    $BUNEXCUTE x prettier internal/view/ --write
-    $BUNEXCUTE x lessc $INPUT_STYLECSS $OUTPUT_STYLECSS
-    $BUNEXCUTE x lessc $INPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
-    $BUNEXCUTE x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_STYLECSS $OUTPUT_STYLECSS
-    $BUNEXCUTE x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
+    $BUN install
+    $BUN x prettier internal/view/ --write
+    $BUN x lessc $INPUT_STYLECSS $OUTPUT_STYLECSS
+    $BUN x lessc $INPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
+    $BUN x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_STYLECSS $OUTPUT_STYLECSS
+    $BUN x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
     exit 0
 fi
 
