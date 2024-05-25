d79be9bea78f012484376c308c43457003f431f7
monirzadeh <25131576+Monirzadeh@users.noreply.github.com>
2024-05-25 15:38:05 +0330

    restore wrong file commit

--- scripts/styles.sh
+++ scripts/styles.sh
@@ -8,12 +8,12 @@ OUTPUT_ARCHIVECSS=internal/view/assets/css/archive.css
 
 # Use bun is installled
 if [ -x "$(command -v bun)" ]; then
-    sde -chip-check-disable -- bun install
-    sde -chip-check-disable -- bun x prettier internal/view/ --write
-    sde -chip-check-disable -- bun x lessc $INPUT_STYLECSS $OUTPUT_STYLECSS
-    sde -chip-check-disable -- bun x lessc $INPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
-    sde -chip-check-disable -- bun x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_STYLECSS $OUTPUT_STYLECSS
-    sde -chip-check-disable -- bun x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
+    bun install
+    bun x prettier internal/view/ --write
+    bun x lessc $INPUT_STYLECSS $OUTPUT_STYLECSS
+    bun x lessc $INPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
+    bun x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_STYLECSS $OUTPUT_STYLECSS
+    bun x clean-css-cli $CLEANCSS_OPTS -o $OUTPUT_ARCHIVECSS $OUTPUT_ARCHIVECSS
     exit 0
 fi
 
