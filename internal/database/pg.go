5eda5f6088139cbc1c1189ee2fab242d07c65e26
monirzadeh <25131576+Monirzadeh@users.noreply.github.com>
2024-09-24 13:08:40 +0330

    add mysql logic

--- internal/database/pg.go
+++ internal/database/pg.go
@@ -659,6 +659,7 @@ func (db *PGDatabase) SaveAccount(ctx context.Context, account model.Account) (e
 
 // SaveAccountSettings update settings for specific account  in database. Returns error if any happened
 func (db *PGDatabase) SaveAccountSettings(ctx context.Context, account model.Account) (err error) {
+
 	// Insert account to database
 	_, err = db.ExecContext(ctx, `UPDATE account
    		SET config = $1
