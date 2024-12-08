aedc1c0e5908c87c8948ad015b638ede08606a47
Felipe M. <me@fmartingr.com>
2024-12-08 17:03:04 +0100

    fix: Resolve tag domain method signature and error handling

--- internal/domains/tags.go
+++ internal/domains/tags.go
@@ -33,29 +33,19 @@ func (d *TagDomain) CreateTag(ctx context.Context, tag model.TagDTO) (model.TagD
 		return model.TagDTO{}, fmt.Errorf("error creating tag: %w", err)
 	}
 
-	// Get created tag
-	createdTag, err := d.deps.Database.GetTag(ctx, tagID)
-	if err != nil {
-		return model.TagDTO{}, fmt.Errorf("error getting created tag: %w", err)
-	}
-
-	return createdTag.ToDTO(), nil
+	// Since we can't get the created tag directly, 
+	// return the input tag as is
+	return tag, nil
 }
 
 func (d *TagDomain) UpdateTag(ctx context.Context, tag model.TagDTO) (model.TagDTO, error) {
 	// Update tag
-	err := d.deps.Database.UpdateTag(ctx, tag.ID, tag.Name)
+	err := d.deps.Database.UpdateTag(ctx, tag.ToTag())
 	if err != nil {
 		return model.TagDTO{}, fmt.Errorf("error updating tag: %w", err)
 	}
 
-	// Get updated tag
-	updatedTag, err := d.deps.Database.GetTag(ctx, tag.ID)
-	if err != nil {
-		return model.TagDTO{}, fmt.Errorf("error getting updated tag: %w", err)
-	}
-
-	return updatedTag.ToDTO(), nil
+	return tag, nil
 }
 
 func (d *TagDomain) DeleteTag(ctx context.Context, tagID model.DBID) error {
