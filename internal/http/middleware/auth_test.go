2ac9714e2468926dff5bf7fc58f8100101377a6d
Federico Scodelaro <federicoscodelaro@gmail.com>
2025-05-23 21:44:06 -0300

    test: Some test

--- internal/http/middleware/auth_test.go
+++ internal/http/middleware/auth_test.go
@@ -99,6 +99,148 @@ func TestAuthMiddleware(t *testing.T) {
 	})
 }
 
+func TestAuthMiddlewareWithSSO(t *testing.T) {
+	logger := logrus.New()
+	_, deps := testutil.GetTestConfigurationAndDependencies(t, context.TODO(), logger)
+	deps.Config().Http.SSOEnable = true
+
+	account, err := deps.Domains().Accounts().CreateAccount(context.TODO(), model.AccountDTO{
+		ID: model.DBID(98),
+		Username: "test_username",
+		Password: "super_secure_password",
+	})
+	require.NoError(t, err)
+
+	t.Run("test no authorization method", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err := middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.Nil(t, c.GetAccount())
+	})
+
+	t.Run("test authorization header", func(t *testing.T) {
+		account := testutil.GetValidAccount().ToDTO()
+		token, err := deps.Domains().Auth().CreateTokenForAccount(&account, time.Now().Add(time.Minute))
+		require.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.Header.Set(model.AuthorizationHeader, model.AuthorizationTokenType+" "+token)
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err = middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.NotNil(t, c.GetAccount())
+	})
+
+	t.Run("test authorization cookie", func(t *testing.T) {
+		account := model.AccountDTO{Username: "shiori"}
+		token, err := deps.Domains().Auth().CreateTokenForAccount(&account, time.Now().Add(time.Minute))
+		require.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.AddCookie(&http.Cookie{
+			Name:   "token",
+			Value:  token,
+			MaxAge: int(time.Now().Add(time.Minute).Unix()),
+		})
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err = middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.NotNil(t, c.GetAccount())
+	})
+
+	t.Run("test invalid token cookie is removed", func(t *testing.T) {
+		// Create an invalid token
+		invalidToken := "invalid-token"
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.AddCookie(&http.Cookie{
+			Name:   "token",
+			Value:  invalidToken,
+			MaxAge: int(time.Now().Add(time.Minute).Unix()),
+		})
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err := middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.Nil(t, c.GetAccount())
+
+		// Check that the token cookie was removed in the response
+		responseCookies := w.Result().Cookies()
+
+		var tokenCookie *http.Cookie
+		for _, cookie := range responseCookies {
+			if cookie.Name == "token" {
+				tokenCookie = cookie
+				break
+			}
+		}
+
+		require.NotNil(t, tokenCookie, "Token cookie should exist in response")
+		require.Empty(t, tokenCookie.Value, "Token cookie value should be empty")
+	})
+	t.Run("test untrusted ip", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.RemoteAddr = "invalid-ip"
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err := middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.Nil(t, c.GetAccount())
+	})
+
+	t.Run("test empty header", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.RemoteAddr = "10.0.0.3"
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err := middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.Nil(t, c.GetAccount())
+	})
+
+	t.Run("test invalid sso username", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.RemoteAddr = "10.0.0.3"
+		r.Header.Add("Remote-User", "username")
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err := middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.Nil(t, c.GetAccount())
+	})
+
+	t.Run("test sso login", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.RemoteAddr = "10.0.0.3"
+		r.Header.Add("Remote-User", account.Username)
+		c := webcontext.NewWebContext(w, r)
+
+		middleware := NewAuthMiddleware(deps)
+		err := middleware.OnRequest(deps, c)
+		require.NoError(t, err)
+		require.NotNil(t, c.GetAccount())
+	})
+}
+
 func TestRequireLoggedInUser(t *testing.T) {
 	logger := logrus.New()
 	_, deps := testutil.GetTestConfigurationAndDependencies(t, context.TODO(), logger)
